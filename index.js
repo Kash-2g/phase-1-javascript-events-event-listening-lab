48211b6b1b45476bb14a2d38dc66df68b53ce5cd
Kash-2g <162958950+Kash-2g@users.noreply.github.com>
2024-04-08 14:08:52 +0300

    final commit

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+    });
 }
+
+// Call the function to bind the event listener
+addingEventListener();
